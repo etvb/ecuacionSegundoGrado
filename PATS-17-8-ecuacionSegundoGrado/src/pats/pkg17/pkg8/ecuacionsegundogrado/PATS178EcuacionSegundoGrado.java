b43dec65ee344ab5d41f2ec9053bcba2800080d0
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-08-31 06:05:56 -0500

    archivos base, agregamos als variables

---
+++ PATS-17-8-ecuacionSegundoGrado/src/pats/pkg17/pkg8/ecuacionsegundogrado/PATS178EcuacionSegundoGrado.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pats.pkg17.pkg8.ecuacionsegundogrado;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS178EcuacionSegundoGrado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        float a, b, c, x;
+        
+        
+        
+        
+        
+    }
+    
+}
